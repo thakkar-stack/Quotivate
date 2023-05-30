00df8c1206c13500415c6b449d587189a0792ca4
Aditya Thakkar <63272852+thakkar-stack@users.noreply.github.com>
2023-05-30 13:21:12 +1000

    idk what im doing

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import random
-
 from quotes import quotes
+from quotes import genres
 
 #app idea to give a quote depening on the type of genre selected by user,
 #could be philosophical, about love, sadness, existentialism, etc.
 
 def genre_of_quote():
-    quote=random.choice(quotes)
     

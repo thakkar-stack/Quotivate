cc8176fbee6b8466d7c902ac5e4edd371c1a2a89
Aditya Thakkar <63272852+thakkar-stack@users.noreply.github.com>
2023-06-01 17:17:59 +1000

    added user input to allow choice of genre of quote

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import random
-from quotes import quotes
-from quotes import genres
+import quotes
 
 #app idea to give a quote depening on the type of genre selected by user,
 #could be philosophical, about love, sadness, existentialism, etc.
 
-def genre_of_quote():
-    
+def main():
+    whichGenre()
+
+
+def whichGenre():
+    genre = input('Which genre of quote would you like?:')
+    if genre == (quotes.genres[0]):
+        print(random.choice(quotes.motivational))
+    elif genre == (quotes.genres[1]):
+        print(random.choice(quotes.philosophical))
+        
+main()

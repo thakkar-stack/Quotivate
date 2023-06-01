67064c9eb56ad9cdd8831dcc0c8a38966831f006
Aditya Thakkar <63272852+thakkar-stack@users.noreply.github.com>
2023-06-01 17:17:24 +1000

    created small quote bank to randomly choose from

--- quotes.py
+++ quotes.py
@@ -1,5 +1,7 @@
-motivational = ['Me and my niggas tryinna get it ya bish', 'Perfection Is Procrastination Masquerading as Quality Control']
+motivational = ['"Perfection Is Procrastination Masquerading as Quality Control - Chris Williamson"',
+                '"You are what you do, not what you say you will do - Carl Jung"']
 
-philosophical = ['philosophical']
+philosophical = ['"Thinking is difficult, that\'s why most people judge - Carl Jung"',
+                 '"This is what hard feels like, and thats okay - Alex Hormozi"']
 
-genres = [motivational, philosophical, ]
+genres = ['motivational', 'philosophical']

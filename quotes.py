c40c486897d99cc94a752008801a66c61cfa7949
Aditya Thakkar <63272852+thakkar-stack@users.noreply.github.com>
2023-06-05 14:47:10 +1000

    added more quotes

--- quotes.py
+++ quotes.py
@@ -1,7 +1,15 @@
-motivational = ['"Perfection Is Procrastination Masquerading as Quality Control - Chris Williamson"',
-                '"You are what you do, not what you say you will do - Carl Jung"']
+motivational = ['"Perfection Is Procrastination Masquerading as Quality Control" - Chris Williamson',
+                '"This is what hard feels like, and that\'s okay" - Alex Hormozi', 
+                '"The only way to do great work is to love what you do" - Steve Jobs',
+                '"You miss 100% of the shots you don\'t take" - Wayne Gretzky',
+                '"The best revenge is massive success" - Frank Sinatra',
+                ]
 
-philosophical = ['"Thinking is difficult, that\'s why most people judge - Carl Jung"',
-                 '"This is what hard feels like, and thats okay - Alex Hormozi"']
+philosophical = ['"Thinking is difficult, that\'s why most people judge" - Carl Jung',
+                 '"You are what you do, not what you say you will do" - Carl Jung',
+                 '"In the middle of every difficulty lies opportunity" - Albert Einstein',
+                 '"The greatest glory in living, lies not in falling, but in rising every time we fall" - Nelson Mandela',
+                 '"Don\'t be pushed around by the fears in your mind. Be lead by the dreams in your heart" - Roy T. Bennett',
+                 ]
 
 genres = ['motivational', 'philosophical']
